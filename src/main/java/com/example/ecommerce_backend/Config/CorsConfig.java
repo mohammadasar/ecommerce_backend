ad48bc83e3000708d917c4dae5999d248bad268a
mohammadasar <itsmemdasar415@gmail.com>
2025-06-17 12:50:29 +0530

    modified corsconfi file

--- src/main/java/com/example/ecommerce_backend/Config/CorsConfig.java
+++ src/main/java/com/example/ecommerce_backend/Config/CorsConfig.java
@@ -16,12 +16,13 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of(
-        	    "http://localhost:8080",
-        	    "http://localhost:5173",
-        	    "http://127.0.0.1:5500",
-        	   "https://devwerxoil.netlify.app"
-        	));
+//        config.setAllowedOrigins(List.of(
+//        	    "http://localhost:8080",
+//        	    "http://localhost:5173",
+//        	    "http://127.0.0.1:5500",
+//        	   "https://devwerxoil.netlify.app"
+//        	));
+        config.setAllowedOrigins(List.of("https://devwerxoil.netlify.app")); // ✅ Allow your Netlify domain
         config.setAllowedHeaders(List.of("Origin", "Content-Type", "Accept", "Authorization"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
